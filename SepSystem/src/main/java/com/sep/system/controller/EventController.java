6baee0011ea3c2d9e3985f0fa09afb0cb8b4d5f6
Spycsh <757407490@qq.com>
2020-10-11 17:40:16 +0200

    state of event flow

--- SepSystem/src/main/java/com/sep/system/controller/EventController.java
+++ SepSystem/src/main/java/com/sep/system/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.sep.system.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -8,12 +9,14 @@ import java.util.Optional;
 import com.sep.system.model.Event;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import static java.lang.Integer.parseInt;
 
@@ -40,13 +43,57 @@ public class EventController {
 
 
     @PostMapping(value = "/editEvent")
-    public String editEvent(@RequestParam("eventId") String eventId, Map<String, Object> map){
+    public String editEvent(HttpServletRequest request ,@RequestParam("eventId") String eventId, Map<String, Object> map){
 //        List<Event> event = eventRepository.findById(eventId);
         System.out.println(eventId);
 
         Event event = eventRepository.findById(parseInt(eventId)).get();
 
         map.put("event", event);
+
+
+
+        HttpSession session = request.getSession();
+        String role = (String) session.getAttribute("role");
+
+        // default view setting
+        session.setAttribute("budgetCommentView", "disabled");
+        session.setAttribute("choiceView", "disabled");
+
+        // change the view depending on role and status in edit page
+        // if the status is closed, then no one can edit it any longer
+        if(!event.getStatus().equals("Closed")){
+            // if the status is not closed, then AM and SCSM can edit "reject" or "approve" choice
+            // only when AM
+            switch(role){
+                case "senior customer service manager":
+                    if(event.getStatus().equals("draft")){
+//                        System.out.println("aaaaaaa");
+                        session.setAttribute("budgetCommentView", "disabled");
+                        session.setAttribute("choiceView", "choice");
+                    }
+                    break;
+                case "admin manager":
+                    if(event.getStatus().equals("FMcommented")){
+                        session.setAttribute("budgetCommentView", "disabled");
+                        session.setAttribute("choiceView", "choice");
+                    }
+                    break;
+
+                case "financial manager":
+                    if(event.getStatus().equals("SCSapproved")) {
+                        session.setAttribute("budgetCommentView", "budgetComment");
+                        session.setAttribute("choiceView", "disabled");
+                    }
+                    break;
+                default:
+                    session.setAttribute("budgetCommentView", "disabled");
+                    session.setAttribute("choiceView", "disabled");
+
+
+            }
+        }
+
 
         return "editEventRequest";
 
@@ -61,9 +108,10 @@ public class EventController {
     public String editEvent(HttpServletRequest request, @RequestParam("eventId") String eventId, @RequestParam("comment") String comment, @RequestParam("expectedBudget") int expectedBudget,
                             @RequestParam("choice") String choice){
         System.out.println(eventId);
+        HttpSession session = request.getSession();
 
         // get current role
-        String role = (String) request.getSession().getAttribute("role");
+        String role = (String) session.getAttribute("role");
         // get the event currently dealt with
         Event event = eventRepository.findById(parseInt(eventId)).get();
 
@@ -72,17 +120,27 @@ public class EventController {
         }else{
             switch (role) {
                 case "admin manager":
-                    if(event.getStatus().equals("SCSapprove"))
-                        event.setStatus("AMapprove");
+                    if(event.getStatus().equals("FMcommented")){
+                        event.setStatus("AMapproved");
+
+                    }
+
                     break;
                 case "senior customer service manager":
-                    if(event.getStatus().equals("draft"))
-                        event.setStatus("SCSapprove");
+                    if(event.getStatus().equals("draft")){
+                        event.setStatus("SCSapproved");
+
+                    }
+
                     break;
                 case "financial manager":
                     event.setComment(comment);
-                    event.setExpectedBudget(expectedBudget);
+                    event.setStatus("FMcommented");
+//                    event.setExpectedBudget(expectedBudget);
+
                     break;
+
+
             }
         }
 
