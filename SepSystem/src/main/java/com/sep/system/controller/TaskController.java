c505b8078946d901ae25d930e8cfc59a1249c8a6
Spycsh <757407490@qq.com>
2020-10-13 01:55:16 +0200

    edit, task status, some bugs and css

--- SepSystem/src/main/java/com/sep/system/controller/TaskController.java
+++ SepSystem/src/main/java/com/sep/system/controller/TaskController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.sep.system.model.Employee;
+import com.sep.system.model.Event;
 import com.sep.system.model.Task;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import static java.lang.Integer.parseInt;
 
 @Controller
 public class TaskController {
@@ -62,7 +65,7 @@ public class TaskController {
 
 
     @PostMapping(value = "/createTaskRequest")
-    public String createEvent(@RequestParam("eventId") int eventId, @RequestParam("employeeId") int employeeId,
+    public String createEvent( @RequestParam("eventId") int eventId, @RequestParam("employeeId") int employeeId,
                               @RequestParam("subject") String subject, @RequestParam("description") String description,
                               @RequestParam("priority") String priority,
                               Map<String, Object> map,HttpServletRequest request) {
@@ -78,18 +81,73 @@ public class TaskController {
 
     }
 
+    @PostMapping(value = "/editTask")
+    public String editTask(HttpServletRequest request, @RequestParam("taskId") String taskId, Map<String, Object> map){
+        HttpSession session = request.getSession();
+        String role = (String) session.getAttribute("role");
+
+        if(role.equals("production manager")|role.equals("service manager")){
+            map.put("commentView", "disabled");     // for managers, they can only view the comment
+        }else{
+            map.put("commentView", "commentView");  // for ordinary team members, they can comment
+        }
+        System.out.println(taskId);
+        Task task = taskRepository.findById(parseInt(taskId)).get(0);
+
+        map.put("task", task);
+
+
+        return "editTaskRequest";
+
+    }
+
+    // click the confirm button of task
+    @PostMapping(value= "/editTaskRequest")
+    public String editTaskRequest(HttpServletRequest request, Map<String, Object> map,
+                                  @RequestParam("status") String status,
+                                  @RequestParam("taskId") String taskId,
+                                  @RequestParam("comment") String comment){
+
+        // change the status of task
+        // Draft=>Edited=>Reviewed
+        HttpSession session = request.getSession();
+        String role = (String) session.getAttribute("role");
+        if(role.equals("production manager")|role.equals("service manager")){
+            if(status.equals("Edited")){
+                status = "Checked";
+            }
+        }else{
+            if(status.equals("Draft")){
+                status = "Edited";
+            }
+        }
+
+        // save the comment for the task in the database
+        // find the task
+        Task task = taskRepository.findById(parseInt(taskId)).get(0);
+        task.setComment(comment);
+        task.setStatus(status);
+        taskRepository.save(task);
+
+        return "redirect:/taskManagement";
+
+    }
+
+
+
      //for insert a few data into the database
      @RequestMapping(value = "/insertTestTask")
      public String insertTestTask(){
          List<Task> taskList = new ArrayList<Task>();
-         taskList.add(new Task(0, 7, "flower photo", "take a picture of flower", 6, "production", "high", "draft","tobias"));
-         taskList.add(new Task(0, 9, "cook dinner", "cook beef with wine", 9, "service", "medium", "draft","helene"));
+         taskList.add(new Task(0, 7, "flower photo", "take a picture of flower", 6, "production", "high", "Draft","tobias"));
+         taskList.add(new Task(0, 9, "cook dinner", "cook beef with wine", 9, "service", "medium", "Draft","helene"));
          for(Task t : taskList){
              taskRepository.save(t);
- 
+
          }
-         
+
          return "redirect:success";
- 
+
      }
+
 }
